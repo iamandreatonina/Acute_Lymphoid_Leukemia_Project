66cd51b72ba2e6624ffa54791944ea0d8ad868c3
iamandreatonina <andrea.tonina97@gmail.com>
2024-03-16 12:04:48 +0100

    pulizie di primavera

---
+++ Code/Expansion_Pomzp3.R
@@ -0,0 +1,36 @@
+
+library(dplyr)
+# Set the working directory to the folder containing the expansion files
+
+# Initialize vectors to store the X values and corresponding file names
+x<-c()
+y<-c()
+index=1
+
+# Loop through each file in the directory
+for (file in list.files()){
+  print(file)
+  # Read the current file
+    expanded<-read.csv(file,sep =',',header = T)
+  # Filter for rows where X0 is "pomzp3"
+  present<-expanded %>% dplyr::filter(expanded$X0 == "pomzp3")
+  # Check if the 'present' dataframe is not empty
+  if (!is_empty(present$X)){
+  # Add the X value and file name to the vectors
+  x[index]<-c(present$X)
+  y[index]<-c(file)
+  index=index+1
+  # }
+  }
+}
+
+# Remove the ".csv" extension from the file names
+new_y<-str_remove(y, ".csv")
+
+# Create a new dataframe with the X values and modified file names
+data<-data.frame(X=x, Y=new_y)
+
+# Write the new dataframe to a CSV file without row names
+write.csv(data,file='Where_POMZP3.csv',row.names = F)
+
+#POMZP3 <- read.csv('Where_POMZP3.csv') no POMZP3 for 99%, need to check 90% 
---
+++ Code/Gene_exapnsions.py
@@ -0,0 +1,205 @@
+import networkx as nx
+import pandas as pd 
+import matplotlib.pyplot as plt 
+import os 
+
+
+directory = '/home/andrea/Desktop/Acute_Lymphoid_Leukemia_Project/data/Expantion' 
+nodes=[]
+edges = []
+
+human_spec = pd.read_csv('../data/Selected_genes_for_expansion_LBDM.txt',header= 0)
+
+
+lista_genes=[]
+
+'''Changed the treshold to 90% from 99%'''
+
+for dirpath, dirnames, filenames in os.walk(directory): 
+    lista_genes_2=[]
+    for filename in filenames: 
+        #lista_genes_2 =[]
+        gene = ''
+        #print(dirpath)
+        if filename.endswith('.expansion'): 
+            print(filename)
+            dataset = pd.read_csv(dirpath+"/"+filename,header= 1,skiprows=0)
+            for i in range(len(dataset)):
+                if dataset.Fabs.iloc[i] >= (2000*0.90):
+                    nodes.append(dataset.node.iloc[i])
+                    lista_genes.append(dataset.node.iloc[i])
+                    lista_genes_2.append(dataset.node.iloc[i])
+
+        else:
+            dataset_2 = pd.read_csv(dirpath+"/"+filename,skiprows=0,header=1)
+            print(filename)
+            nodes.append(dataset_2.x.value_counts().idxmax())
+            nodes = list(set(nodes))
+            gene = dataset_2.x.value_counts().idxmax()
+            for j in range(len(dataset_2)):
+                 if dataset_2.Fabs.iloc[j] >= (2000*0.90):
+                    momentary = (dataset_2.x.iloc[j],dataset_2.y.iloc[j],int(dataset_2.Fabs.iloc[j])/2000)
+                    edges.append(momentary)
+                    if dataset_2.x.iloc[j] not in lista_genes:
+                        lista_genes.append(dataset_2.x.iloc[j])
+                        #lista_genes_2.append(dataset_2.x.iloc[j])
+                    if dataset_2.y.iloc[j] not in lista_genes:    
+                        lista_genes.append(dataset_2.y.iloc[j])
+                        #lista_genes_2.append(dataset_2.y.iloc[j])
+
+        print(lista_genes_2)
+        if len(lista_genes_2) != 0: 
+            paperino = pd.DataFrame(lista_genes_2)
+            paperino.to_csv('../data/Meaningfull_expantion_90%/'+gene+'.csv', index= True) 
+
+# print(edges)
+# print(nodes)
+
+G = nx.DiGraph()
+G.add_nodes_from(nodes)
+G.add_weighted_edges_from(edges)
+
+
+color_map =[]
+for n in G:
+    if n.upper() in list(human_spec['Human_specific']):
+        color_map.append('orange')
+    else:
+        color_map.append('blue')
+
+    
+# nx.draw(G,with_labels=True, node_color = color_map)
+# plt.show()
+
+node_degree_dict = nx.degree(G)
+selected = [x for x in G.nodes() if node_degree_dict[x] > 3]
+print(len(selected))
+G2 =G.subgraph(selected)
+
+color_map2 =[]
+for n in G2:
+    if n.upper() in list(human_spec['Human_specific']):
+        color_map2.append('orange')
+    else:
+        color_map2.append('blue')
+
+#nx.draw(G2,with_labels=True, node_color = color_map2)
+#plt.show()
+
+
+
+listona=[]
+for tupla in G2.edges():
+    listona.append(tupla[0])
+    listona.append(tupla[1])
+
+selected2 = [x for x in G2.nodes() if x in listona]
+print(G2.edges())
+
+nodelist=[]
+for node in selected2:
+    nodelist.extend(list(G.neighbors(n=node)))
+    nodelist.append(node)
+
+
+sub=G.subgraph(nodelist)
+color_map3 =[]
+for n in sub:
+    if n.upper() in list(human_spec['Human_specific']):
+        color_map3.append('orange')
+    else:
+        color_map3.append('blue')
+
+
+nx.draw(sub,with_labels=True, node_color = color_map3)
+plt.show()
+
+
+graph_1=['grapl', 'lrrc37a', 'lrrc37a3', 'arl17a', 'stag3']
+
+nodelist_graph1=[]
+
+for node in graph_1:
+    nodelist_graph1.extend(list(G.neighbors(n=node)))
+    nodelist_graph1.append(node)
+
+sub_graph1=G.subgraph(nodelist_graph1)
+color_map4 =[]
+for n in sub_graph1:
+    if n.upper() in list(human_spec['Human_specific']):
+        color_map4.append('orange')
+    else:
+        color_map4.append('blue')
+
+nx.draw(sub_graph1,with_labels=True, node_color = color_map4)
+plt.show()
+
+
+graph_3=['tmem236', 'nrxn3', 'mrc1']
+
+nodelist_graph3=[]
+
+for node in graph_3:
+    nodelist_graph3.extend(list(G.neighbors(n=node)))
+    nodelist_graph3.append(node)
+
+sub_graph3=G.subgraph(nodelist_graph3)
+color_map6 =[]
+for n in sub_graph3:
+    if n.upper() in list(human_spec['Human_specific']):
+        color_map6.append('orange')
+    else:
+        color_map6.append('blue')
+
+nx.draw(sub_graph3,with_labels=True, node_color = color_map6)
+plt.show()
+
+
+
+graph_2=['nbpf14','gtf2i','fam156a']
+
+nodelist_graph2=[]
+
+for node in graph_2:
+    nodelist_graph2.extend(list(G.neighbors(n=node)))
+    nodelist_graph2.append(node)
+
+sub_graph2=G.subgraph(nodelist_graph2)
+color_map5 =[]
+for n in sub_graph2:
+    if n.upper() in list(human_spec['Human_specific']):
+        color_map5.append('orange')
+    else:
+        color_map5.append('blue')
+
+nx.draw(sub_graph2,with_labels=True, node_color = color_map5)
+plt.show()
+
+
+print(lista_genes)
+dataframe=pd.DataFrame(lista_genes)
+print(dataframe)
+dataframe.to_csv('../data/Expansion_genes_90%.csv', index=True)
+
+nodelist_HS=[]
+for gene in list(human_spec['Human_specific']):
+    nodelist_HS.extend(list(G.neighbors(n=gene.lower())))
+    nodelist_HS.append(gene.lower())
+    dataframe=pd.DataFrame(nodelist_HS)
+    dataframe.to_csv('../data/Expansion_files_90%/'+gene+'.csv', index=True)
+
+
+'''capire meglio questione grafi'''
+
+dataframe=pd.DataFrame(nodelist_graph1)
+# dataframe.to_csv('../data/Graphs_csv_90%/Graph1_5HS.csv', index=True)
+
+
+
+dataframe=pd.DataFrame(nodelist_graph2)
+# dataframe.to_csv('../data/Graphs_csv_90%/Graph2_3HS.csv', index=True)
+
+
+
+dataframe=pd.DataFrame(nodelist_graph3)
+# dataframe.to_csv('../data/Graphs_csv_90%/Graph3_3HS.csv', index=True)
